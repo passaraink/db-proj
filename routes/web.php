9393463e36176840ff0ed5e95dbdec62c4bbaa8e
nartra <nartra@gmail.com>
2020-08-22 18:03:31 +0700

    Auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/test', function(){
+    return \App\Models\User::all();
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
